5d6b04532a970ba7862a158b81189b626c42da3a
jasmeen786 <148907683+jasmeen786@users.noreply.github.com>
2024-04-09 10:39:36 -0600

    Delete my-project directory

--- my-project/models.js
+++
@@ -1,9 +0,0 @@
-const models = {
-    saveRegistration: (registrationData) => {
-      // Save registration data to the database
-      console.log('Registration data saved:', registrationData);
-    }
-  };
-  
-  module.exports = models;
-  
--- my-project/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controllers = require('./controllers');
-
-// Define routes
-router.post('/register', controllers.register);
-
-module.exports = router;
